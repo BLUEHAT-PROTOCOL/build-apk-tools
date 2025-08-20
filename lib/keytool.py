c899e58f155ce406df5514a2399a30e151d7b295
FAHDiL.github.com <f50489341@gmail.com>
2025-08-20 10:51:09 +0700

    Update keytool.py

--- lib/keytool.py
+++ lib/keytool.py
@@ -1,14 +1,13 @@
 import os, subprocess, getpass
 
 def make_keystore():
-    alias = "release"
-    ks = "release.keystore"
-    if os.path.exists(ks):
+    if os.path.exists("release.keystore"):
         return
     pw = getpass.getpass("Masukkan password keystore: ")
     dname = "CN=EyeFox, OU=Mobile, O=Android, L=Jakarta, C=ID"
     subprocess.run([
-        "keytool","-genkey","-v","-keystore",ks,"-alias",alias,
-        "-keyalg","RSA","-keysize","2048","-validity","10000",
-        "-dname",dname,"-storepass",pw,"-keypass",pw
+        "keytool", "-genkey", "-v", "-keystore", "release.keystore", "-alias", "release",
+        "-keyalg", "RSA", "-keysize", "2048", "-validity", "10000",
+        "-dname", dname, "-storepass", pw, "-keypass", pw
     ])
+    
