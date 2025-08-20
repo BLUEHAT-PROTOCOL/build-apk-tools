0330400e26b7257a6a0e60f7a0012f35f0e590dc
FAHDiL.github.com <f50489341@gmail.com>
2025-08-20 10:50:41 +0700

    Update colors.py

--- lib/colors.py
+++ lib/colors.py
@@ -1,7 +1,4 @@
-# 256-color helper untuk terminal Termux
-# Usage: print(f"{c1}teks{c0}")
-
-c0 = "\033[0m"      # reset
-c1 = "\033[1;96m"   # cyan bold  (judul box)
-c2 = "\033[1;92m"   # green bold (nilai aktif)
-c3 = "\033[1;93m"   # yellow     (label)
+c0 = "\033[0m"
+c1 = "\033[1;96m"
+c2 = "\033[1;92m"
+c3 = "\033[1;93m"
